1a7423b4351d0dd3b659f591d30271f580931436
VenusDragon <b05202024@ntu.edu.tw>
2024-05-15 15:58:53 +0800

    mini fix

--- compute_all_subset.py
+++ compute_all_subset.py
@@ -3,8 +3,10 @@ import json
 import numpy as np 
 from itertools import combinations
 from tqdm import tqdm
-partial_gradient_norm  = [118.91933550356596, 25.62523640515409, 18.178103280602798, 3.5722913263428637, 6.101069767894579, 20.675488120230625, 5.296538376569728, 0.409159135144364, 0.2640024833699073, 0.13404464475232203, 1.3722768333581072, 7.64251616516276e-05, 5.299537518078623, 1.6871743462766489]
-folder_path = './instance_3_vi_data'
+from trans import length_2_subset_dependency
+partial_gradient_norm  = [323.55596783123735, 16.64309301545677, 20.664327935240934, 0.7640419085988084, 8.322414016557218, 36.0900489996741, 2.3600512407406358, 2.023149132555818, 0.3237797476085129, 1.722481500606519, 0.10121379279226961, 0.00015068922881678048, 0.9322766973094891, 6.387281114990409]
+index = 5
+folder_path = f"./instance_{index}_vi_data"
 file_extension = 'res.json'
 params_list = ['X_-TACT_TIME_mean', 'X_-CONVEYOR_SPEED_mean', 'PUMP_high', 'PUMP_low', 'CLN1_over-etching-ratio', 'CLN1_EPT_time', 'clean_count', 'EPT_clean_count_ratio', 'NH3_TREAT_-RF_FREQ-max', 'NH3_TREAT_-RF_FREQ-range', 'NH3_TREAT_-RF_FREQ-mean', 'NP_3_-MFC_VOL_SIH4-range', 'VENT_high', 'VENT_low', 'DFT_CNT']
 def generate_all_subsets(num):                     #
@@ -33,7 +35,7 @@ for json_file in json_files:
 AUO_coalitions = generate_all_subsets(14)
 length_2_coalitions = [coalition for coalition in AUO_coalitions if len(coalition)<=2]
 residual_norm_of_each_subset = {}
-for coalition in tqdm(length_2_coalitions):        #對所有subsets
+for coalition in tqdm(length_2_coalitions ):        #對所有subsets
     if len(coalition)!=0:
         residual_vector_sum = {}
         partial_gradient_subset_norm = 0
@@ -52,9 +54,11 @@ for coalition in tqdm(length_2_coalitions):        #對所有subsets
         residual_norm_of_each_subset[str(coalition)] = (residual_subset_norm/partial_gradient_subset_norm)**0.5
 
 print(residual_norm_of_each_subset)
-saved_file_name = 'instance_3_length_2_coalitions_scaled_norm.json'
+saved_file_name = f'./instance_{index}_vi_data/instance_{index}_length_2_scaled_norm.json'
 with open(saved_file_name, 'w') as json_file:
     json.dump(residual_norm_of_each_subset, json_file)
+    
+length_2_subset_dependency(index=index)
 #print(res_vector_data)
 #for json_file in json_files:
     #file_path = os.path
--- trans.py
+++ trans.py
@@ -3,29 +3,31 @@ import json
 import numpy as np
 import csv
 import re
-index = 4
-file_name = f'instance_{index}_vi_data/instance_{index}_length_2_scaled_norm.json'
-rows = 14
-cols = 14
-# 建立一個空的二維矩陣
-my_matrix = np.zeros((14, 14))
-with open(file_name) as f:
-    data = json.load(f)
-for key, value in data.items():
-    #print(key)
-    numbers = [int(num) for num in re.findall(r'\d+', key)]
-    if len(numbers) ==2:
-        #print(numbers[0],numbers[1])
-        my_matrix[numbers[0]][numbers[1]] = value
+def length_2_subset_dependency(index=4,feature_num=14):
+    '''transform length 2 subset scaled norm to matrix and csv file'''
+    
+    file_name = f'instance_{index}_vi_data/instance_{index}_length_2_scaled_norm.json'
+    rows = feature_num
+    cols = feature_num
+    # 建立一個空的二維矩陣
+    my_matrix = np.zeros((rows, cols))
+    with open(file_name) as f:
+        data = json.load(f)
+    for key, value in data.items():
+        #print(key)
+        numbers = [int(num) for num in re.findall(r'\d+', key)]
+        if len(numbers) ==2:
+            #print(numbers[0],numbers[1])
+            my_matrix[numbers[0]][numbers[1]] = value
 
 
-# 下載路徑
-csv_file = f"./instance_{index}_vi_data/instance_{index}_length_2_residual.csv"
+    # 下載路徑
+    csv_file = f"./instance_{index}_vi_data/instance_{index}_length_2_residual.csv"
 
-# 矩陣寫入csv
-with open(csv_file, mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerows(my_matrix)
+    # 矩陣寫入csv
+    with open(csv_file, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(my_matrix)
 
-print("CSV 文件保存成功！")
+    print("CSV 文件保存成功！")
 
