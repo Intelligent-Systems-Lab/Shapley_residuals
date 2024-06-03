9fef9529452a77df562076853d30b3b24258e4c0
VenusDragon <b05202024@ntu.edu.tw>
2024-06-03 08:52:26 +0800

    renew some function

--- trans.py
+++ trans.py
@@ -3,8 +3,10 @@ import json
 import numpy as np
 import csv
 import re
+import matplotlib.pyplot as plt
 from itertools import combinations
-
+from feature_name import feature_name
+from partial_gradient_square import partial_gradient_square
 def length_2_subset_dependency(index=4,feature_num=14):
     '''transform length 2 subset scaled norm to matrix and csv file'''
     
@@ -98,6 +100,10 @@ def find_global_min(index,subset_len,non_consider_feature=[]):
     for item in keys_smallest_10_percent:
         print(item)
 
+#######
+#######
+'''                 For subset              '''
+
 def trans_json_to_dictinary(json_file_name):
 
     with open(json_file_name) as f:
@@ -107,7 +113,7 @@ def trans_json_to_dictinary(json_file_name):
     
 
 def create_different_len_subset_list(data,feature_num=14,non_consider_feature=[]):
-    ''' input dictionary and show the graph'''
+    ''' input dictionary and output list'''
     flag = True
     different_len_subset_list = [{} for _ in range(feature_num)]        #dictionaries for different length
     for key, value in data.items():
@@ -120,8 +126,82 @@ def create_different_len_subset_list(data,feature_num=14,non_consider_feature=[]
             different_len_subset_list [len(numbers)-1][key] = value         
     return different_len_subset_list
 
+def instance_data_to_subset_list(index,feature_num=14):
+    '''input index and out put list'''
+    file_name = f'./instance_{index}_vi_data/instance_{index}_allsubsets_scaled_norm.json'
+    dict = trans_json_to_dictinary(file_name)
+    data = create_different_len_subset_list(dict,feature_num=feature_num,non_consider_feature=[])
+    return data
+
 def generate_all_subsets(num):                     #
     num_set = [i for i in range(num)]
     all_subsets = [np.array(s) for r in range(num+1) for s in combinations(num_set, r) ]
     # Create a hash table to store the index of each subset
     return all_subsets
+
+def show_fig_single_feature(idx_list,feature_num=14):
+
+    ''' input instances_index_list and show scattered graph of single feature from different instances'''
+
+    stored_list = [[] for _ in range(feature_num)]
+    for idx in idx_list:
+        data = instance_data_to_subset_list(idx)
+        # store each feature scaled norm
+        for i  in range(feature_num):
+            values = list(data[0].values())
+            stored_list[i].append(values[i])
+        #print(stored_list)
+    colors = plt.cm.tab20.colors
+    for i, group in enumerate(stored_list):
+        x_values = [feature_name[i]] * len(group)  # 將 x 軸值調整為同一組的索引值，讓點排列在同一條直線上
+        plt.scatter(x_values, group, color=colors[i % len(colors)])
+
+    
+    plt.xlabel('Feature')
+    plt.ylabel('Scaled norm')
+    plt.title('Single_feature_scaled_norm')
+    plt.xticks(rotation=60, ha='right')  # 將 x 軸標籤旋轉 45 度，並對齊右邊
+    plt.tight_layout()  # 調整圖形布局以避免標籤重疊
+    plt.show()
+
+def show_different_subset_graph(index,subset_len=2,feature_num=14):
+    
+    data = instance_data_to_subset_list(index)
+    subset_idx = subset_len - 1
+    values = list(data[subset_idx].values())
+    plt.ylim(0,1.2)
+    plt.scatter(range(len(values)), values)
+    plt.title(f'length{subset_len}')
+    plt.xlabel('Index')
+    plt.ylabel('Value')
+    plt.show()
+
+### todo
+'''def relative_decreasing_subset(index_list=[],partial_gradient_square=[],subset_len=2,feature_num=14,max_subset_len=6):
+    for index in index_list:
+        file_name = f'./instance_{index}_vi_data/instance_{index}_allsubsets_scaled_norm.json'
+        data = instance_data_to_subset_list(index)
+        dict = trans_json_to_dictinary(file_name)
+        filtered_data = []
+        for i in range(max_subset_len):
+            filtered_dict = {}
+            for key,value in data[i].items():
+                key_list = [int(num) for num in re.findall(r'\d+', key)] 
+                count =0 
+                flag = True
+                partial_gradient_square_sum = sum(partial_gradient_square[index][j] for j in key_list)
+                if i!=0:
+                    subsets= [np.array(subset) for subset in combinations(key_list, i)]
+                    #print(key_list)
+                    #print(subsets)
+                    for subset in subsets:
+                        partial_gradient_square_subset_sum = sum(partial_gradient_square[index][k] for k in subset)
+                        weight = (partial_gradient_square_subset_sum/partial_gradient_square_sum)**0.5 
+                        #print(weight)
+                        if value > dict[str(subset)]*1:
+                            flag = False
+                            #filtered_dict[key] =  value
+                            #print(str(subset))
+                    if flag:
+                        filtered_dict[key] = value
+            filtered_data.append(filtered_dict)    '''
